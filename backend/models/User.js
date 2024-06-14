125378302e8f40aa2bd5d9040e5253e3670d84c6
nikunj <77881638+nik132-eng@users.noreply.github.com>
2024-06-14 16:24:10 +0000

    add is verified in model

--- backend/models/User.js
+++ backend/models/User.js
@@ -32,7 +32,8 @@ var UserSchema = new mongoose.Schema(
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     hash: String,
-    salt: String
+    salt: String,
+    isVerified: Boolean
   },
   { timestamps: true }
 );
