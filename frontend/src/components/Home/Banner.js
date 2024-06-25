f12401995ac03cba9e2d2101316ef5bdc952b2a0
nikunj <77881638+nik132-eng@users.noreply.github.com>
2024-06-25 02:19:17 +0000

    Add a search box to the main page

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,19 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import SearchBox from "./SearchBox";
 
 const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
-        <img src={logo}/>
+        <img src={logo} alt="logo" />
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
           <span> the cool stuff.</span>
+        </div>
+        <div className="mt-4">
+          <SearchBox />
         </div>
       </div>
     </div>
